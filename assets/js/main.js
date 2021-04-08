b68d9c93c8da0cc3a69d88c948a0b12e2dde9985
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-04-08 16:52:21 +0200

    bonus terminati

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,18 @@ var app = new Vue({
     el: '#root',
     data:{
         email: [],
+        emailTwo: [],
+    },
+    created(){
+            
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) =>{
+                this.emailTwo.push(response.data.response)
+            })
+        }
+            console.log(this.emailTwo)
+        
     },
     methods:{
         mailGen: function(){
